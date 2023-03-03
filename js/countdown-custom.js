4cf77d62ae401a9eefcdca61bd9e49004fc3187c
Devansh Kapri <devanshkapri19@gmail.com>
2023-03-04 00:51:03 +0530

    artist reveal

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -5,3 +5,23 @@ jQuery(document).ready(function () {
     }); // year, month, date, hour
   });
 });
+
+// Targeting video element
+let clip = document.querySelector("#artist");
+
+/* Applying mouseover event on video clip 
+and then we call play() function to play 
+the video when the mouse is over the video */
+clip.addEventListener("mouseover", function (e) {
+  clip.muted = !clip.muted;
+  clip.play();
+});
+
+/* Applying mouseout event on video clip 
+and then we call pause() function to stop 
+the video when the mouse is out the video */
+clip.addEventListener("mouseout", function (e) {
+  clip.muted = !clip.muted;
+  clip.currentTime = 0;
+  clip.pause();
+});
